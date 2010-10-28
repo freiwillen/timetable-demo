597289921d01c6bab28594af48ea8c71d9618814
Freiwillen <fr.willen@gmail.com>
2010-10-28 11:17:22 +0300

    Extending of Array now in anonymous function

--- array.js
+++ array.js
@@ -1,47 +1,37 @@
-Array.prototype.each = function(block){
-  for(var i=0;i<this.length;i++)block(this[i])
-}
-Array.prototype.each_with_index = function(block){
-  for(var i=0;i<this.length;i++)block(this[i],i)
-}
-Array.prototype.map = function(block){
-  var r = new Array()
-  for(var i=0;i<this.length;i++)r.push(block(this[i]))
-  return r
-}
-Array.prototype.find = function(block){
-  var r
-  for(var i=0;i<this.length;i++){
-    if(block(this[i]) == true){
-      r = this[i]
-      break
-    }
+(function(){
+  var my_array_methods = {
+    each: function(block){
+      for(var i=0;i<this.length;i++)block(this[i]);
+    },
+    each_with_index: function(block){
+      for(var i=0;i<this.length;i++)block(this[i],i);
+    },
+    map: function(block){
+      var r = new Array();
+      for(var i=0;i<this.length;i++)r.push(block(this[i]));
+      return r;
+    },
+    find: function(block){
+      var r;
+      for(var i=0;i<this.length;i++){
+        if(block(this[i]) == true){ r = this[i]; break;}
+      }
+      return r;
+    },
+    my_reverse: function(){
+      var r = new Array();
+      for(var i=this.length-1;i>=0;i--){ r.push(this[i]);}
+      return r;
+    },
+    index: function(el){
+      for(var i=0;i<this.length;i++){ if(this[i] == el)return i;}
+    },
+    rindex: function(el){ return this.my_reverse().index(el);},
+    first: function(){ return this[0];},
+    last: function(){return this[this.length-1];},
+    any: function(){return (this.length > 0);}
   }
-  return r
-}
-Array.prototype.my_reverse = function(){
-  var r = new Array();
-  for(var i=this.length-1;i>=0;i--){
-    r.push(this[i])
+  for(var key in my_array_methods){
+    Array.prototype[key] = my_array_methods[key];
   }
-  return r
-}
-Array.prototype.index = function(el){
-  for(var i=0;i<this.length;i++){
-    if(this[i] == el)return i
-  }
-}
-Array.prototype.rindex = function(el){
-  return this.my_reverse().index(el)
-}
-
-Array.prototype.first = function(){
-  return this[0]
-}
-
-Array.prototype.last = function(){
-  return this[this.length-1]
-}
-Array.prototype.any = function(){
-  return (this.length > 0)
-}
+})()
